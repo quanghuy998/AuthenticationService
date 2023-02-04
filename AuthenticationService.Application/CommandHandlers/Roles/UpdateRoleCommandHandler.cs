a383c1f7672ace1d1131b3d9e3950261a89f0da5
DESKTOP-MA601IK\OS <huy.huynhducquang@plenti.com.au>
2023-02-04 09:30:05 +0700

    Add CRUD aggregate

---
+++ AuthenticationService.Application/CommandHandlers/Roles/UpdateRoleCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AuthenticationService.Domain.Aggregates.Roles;
+using AuthenticationService.Infrastructure.CQRS.Commands;
+
+namespace AuthenticationService.Application.CommandHandlers.Roles
+{
+    public class UpdateRoleCommand : ICommand<int>
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+    }
+
+    public class UpdateRoleCommandHandler : ICommandHandler<UpdateRoleCommand, int>
+    {
+        private readonly IRoleRepository roleRepository;
+
+        public UpdateRoleCommandHandler(IRoleRepository roleRepository)
+        {
+            this.roleRepository = roleRepository;
+        }
+
+        public async Task<CommandResult<int>> Handle(UpdateRoleCommand request, CancellationToken cancellationToken)
+        {
+            var role = await roleRepository.FindOneAsync(request.Id);
+            if (role is null)
+                return CommandResult<int>.Error("Role is not existing");
+
+            role.Update(request.Name);
+            await roleRepository.UpdateAsync(role, cancellationToken);
+
+            return CommandResult<int>.Success(role.Id);
+        }
+    }
+}
---
+++ AuthenticationService/Controllers/RoleController.cs
@@ -0,0 +1,97 @@
+﻿using AuthenticationService.Application.CommandHandlers.Roles;
+using AuthenticationService.Application.QueryHandlers.Roles;
+using AuthenticationService.Infrastructure.CQRS.Commands;
+using AuthenticationService.Infrastructure.CQRS.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+
+    public class RoleController : ControllerBase
+    {
+        private readonly IQueryBus queryBus;
+        private readonly ICommandBus commandBus;
+
+        public RoleController(IQueryBus queryBus, ICommandBus commandBus)
+        {
+            this.queryBus = queryBus;
+            this.commandBus = commandBus;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] string name, CancellationToken cancellationToken)
+        {
+            var command = new CreateRoleCommand()
+            {
+                Name = name
+            };
+
+            var result = await commandBus.SendAsync(command, cancellationToken);
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok(result.Response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+        {
+            var query = new GetRolesQuery();
+
+            var result = await queryBus.SendAsync(query, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id, CancellationToken cancellationToken)
+        {
+            var query = new GetRoleByIdQuery()
+            {
+                Id = id
+            };
+
+            var result = await queryBus.SendAsync(query, cancellationToken);
+            if (result is null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, string name, CancellationToken cancellationToken)
+        {
+            var command = new UpdateRoleCommand()
+            {
+                Id = id,
+                Name = name,
+            };
+
+            var result = await commandBus.SendAsync(command, cancellationToken);
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok(result.Response);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id, CancellationToken cancellationToken)
+        {
+            var command = new DeleteRoleCommand()
+            {
+                Id = id
+            };
+
+            var result = await commandBus.SendAsync(command, cancellationToken);
+            if (!result.IsSuccess)
+                return BadRequest(result.Message);
+
+            return Ok(result.Response);
+        }
+    }
+}
